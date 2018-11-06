76a5666079408c8dd623eb819c8f4f0c47de9bcb
Ben J <ilyaksyonov@gmail.com>
2018-11-06 17:51:38 +0300

    006

--- lib/Randomizer/Controller/Multiprint.pm
+++ lib/Randomizer/Controller/Multiprint.pm
@@ -20,6 +20,11 @@ sub upload {
     chomp($dir);
     mkdir $dir, 0755;
 
+    my $loto_type =  $self->param('loto_type');
+
+    my $t_cnf = $self->app->config('ticket')->{5};
+    my $l_cnf = $t_cnf->{include}->{$self->param('loto_type')};
+
     for (1 .. 5) {
         next unless $self->req->upload('file_'.$_)->size;
         my $file = $self->req->upload('file_'.$_);
@@ -27,7 +32,6 @@ sub upload {
             name    => $file->filename,
             column  => $self->param('column_'.$_),
             size    => $file->size,
-            #slurp   => $file->slurp,
         };
         $file->move_to("$dir/$data->{$_}->{name}");
     }
@@ -39,8 +43,19 @@ sub upload {
             my @tmp_arr = split ';', $_;
             my $l = scalar @tmp_arr;
             if (!$max_l || $l > $max_l) {$max_l = $l;}
-            # $max_l = $l if $l > $max_l;
         }
+
+        if ($l_cnf->{regexp_modify}) {
+            my $find = $l_cnf->{regex};
+            my $replace = $param->{substition};
+            $_=~ s/$find/$replace/ee for @ry;
+        }
+
+        my $add_null_row = $self->add_null_row({
+            name     => "$dir/$data->{$k}->{name}",
+            add_null => 1000,
+        });
+
         for (@ry) {
             s/\s+\z//;
             chomp;
@@ -70,71 +85,12 @@ sub upload {
     }
     close FILE;
 
-
-    # my @fh;
-    # for my $k (sort {$a <=> $b} keys %$data) {
-    #     open my $fh, '<', $dir . '/' . $data->{$k}->{name} or die "Unable to open '$_' for reading: $!";
-    #     push @fh, $fh;
-    # }
-    #
-    # open FILE, '>', "$dir/output.csv" or die;
-    #
-    # for (1 .. 5) {
-    #     my $str = $fh[0][$_] . ';' . $fh[1][$_];
-    #     print FILE $str;
-    # }
-
-    # my $ln = 1;
-    # while (grep { not eof } @fh) {
-    #     for my $fh (@fh) {
-    #         if (defined(my $line = <$fh>)) {
-    #             p $line;
-    #             chomp $line;
-    #             # print FILE $line;
-    #             ($ln % (scalar keys %$data)) ? print FILE "$line\n" : print FILE "$line";
-    #         }
-    #     }
-    #     $ln++;
-    # }
-
     $self->render_file(
         'filepath'            => "$dir/output.csv",
         'format'              => 'application/x-download',
         'content_disposition' => 'inline',
         'cleanup'             => 0,
     );
-    # for my $file ( @{$self->req->uploads('files')} ) {
-    #     my $size = $file->size;
-    #     my $name = $file->filename;
-    #     my $row_number = 1;
-    #     push @{$data->{names}}, $name;
-    #     my $str = $file->slurp;
-    #     for ( split "\n", $str ) {
-    #         $data->{content}->{$name}->{$row_number++} = $_;
-    #     }
-    #     $data->{rows} = $row_number if !$data->{rows};
-    # }
-    # open FILE, '>>', 'trash/all.txt' or {
-    #     mkdir 'trash' and `touch all.txt`and open FILE, '>>', 'trash/all.txt'
-    # };
-    #
-    # for my $rn ( 1 .. ($data->{rows} - 1))  {
-    #     my $line;
-    #     for (@{$data->{names}}) {
-    #         $line .= $data->{content}->{$_}->{$rn};
-    #     }
-    #     $line .= "\n";
-    #     print FILE $line;
-    # }
-    #
-    # open FILE2, '<', 'trash/all.txt';
-    # my $f;
-    # while (<FILE2>) {
-    #     $f .= "$_\n";
-    # }
-    # p $f;
-    #
-    # $self->render(text => "-\n$f");
 }
 
 1;
