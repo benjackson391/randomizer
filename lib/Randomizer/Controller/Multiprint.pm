929ac5324efb84cb9f4407655dd6f678ce61b166
Ben J <ilyaksyonov@gmail.com>
2018-11-12 13:12:35 +0300

    011

--- lib/Randomizer/Controller/Multiprint.pm
+++ lib/Randomizer/Controller/Multiprint.pm
@@ -12,6 +12,18 @@ sub main {
     $self->render( template => 'default/multiprint');
 }
 
+sub builder_1 {
+    my ($self, $row) = @_;
+    $row =~ s/\s+\z//;
+    chomp $row;
+    $row .= ';' if $row =~ /^.*[^;]$/;
+    my @row = split ';', $row;
+    my $l = scalar @row;
+    return [$row, $l];
+    #$max_l = $l if $l > $max_l;
+
+}
+
 sub upload {
     my $self = shift;
     my $data;
@@ -20,10 +32,8 @@ sub upload {
     chomp($dir);
     mkdir $dir, 0755;
 
-    my $t_cnf = $self->app->config('ticket')->{5};
-    my $l_cnf = $t_cnf->{include}->{$self->param('loto_type')};
-
-    my $max_rows;
+    my $l_cnf = $self->app->config('ticket')->{5}->{include}->{$self->param('loto_type')};
+    #my $l_cnf = $t_cnf->{include}->{$self->param('loto_type')};
 
     for (1 .. 5) {
         next unless $self->req->upload('file_'.$_)->size;
@@ -32,93 +42,57 @@ sub upload {
             name    => $file->filename,
             column  => $self->param('column_' . $_),
             size    => $file->size,
-            max_l_2 => 0,
+            rows    => [split "\n", $file->slurp],
+            max_col => 0,
         };
-
         $file->move_to("$dir/$data->{$_}->{name}");
     }
 
+    my $max_rows = [sort {$b <=> $a} map {scalar @{$data->{$_}->{rows}}} keys %$data]->[0];
+
     for my $k (sort {$a <=> $b} keys %$data) {
-        tie (my @ry,"Tie::File", $dir . '/'. $data->{$k}->{name} ) or die $!;
         if ($l_cnf->{regexp_modify}) {
-            $self->app->log->debug('regexp_modify');
             my $find = $l_cnf->{regex};
             my $replace = $l_cnf->{substition};
-            $_=~ s/$find/$replace/ee for @ry;
-        }
-        untie @ry;
-    }
-
-
-
-    for my $k (sort {$a <=> $b} keys %$data) {
-        tie (my @ry,"Tie::File", $dir . '/'. $data->{$k}->{name} ) or die $!;
-            my $max_l = 0;
-            for (@ry) {
-                s/\s+\z//;
-                chomp;
-                $_ .= ';' if /^.*[^;]$/;
-                my @row = split ';', $_;
-                my $l = scalar @row;
-                $max_l = $l if $l > $max_l;
+            for (@{$data->{$k}->{rows}}) {
+                $_=~ s/$find/$replace/ee;
             }
-            $data->{$k}->{max_l} = $max_l;
-        untie @ry;
-    }
-
-    $self->app->log->debug(Dumper $data);
-
-    for my $k (sort {$a <=> $b} keys %$data) {
-
-        tie (my @ry,"Tie::File", $dir . '/'. $data->{$k}->{name} ) or die $!;
-
-
-
-        my $i;
-        for (@ry) {
+        }
+        for (@{$data->{$k}->{rows}}) {
             s/\s+\z//;
             chomp;
+            $_ .= ';' if /^.*[^;]$/;
+            my @row = split ';', $_;
+            $data->{$k}->{max_col} = scalar @row if scalar @row > $data->{$k}->{max_col};
+        }
 
+        my $i;
+        for (@{$data->{$k}->{rows}}) {
             my $count = () = $_ =~ /\Q;/g;
-            my @split = split ';', $_;
-            my $scalar = scalar @split;
-            my $diff = $data->{$k}->{max_l} - $count;
-            $self->app->log->debug("$diff = $data->{$k}->{max_l} - $count ( $scalar ) $_") if $i++ < 10;
+            # my $scalar = scalar (split ';', $_);
+            my $diff = $data->{$k}->{max_col} - $count;
             $_ .= ';'x int($diff) if $diff;
-            $_ .= "$data->{$k}->{column};\n";
+            $_ .= "$data->{$k}->{column};";
+            $self->app->log->debug("$diff = $data->{$k}->{max_col} - $count") if $i++ < 10;
         }
 
-        pop @ry unless $ry[-1] =~ /[\w\d]/;
-
-        my $diff = $max_rows - scalar(@ry);
-        $self->app->log->debug("diff $diff max_rows $max_rows  scalar(\@ry) "  .  scalar(@ry));
-        if ($diff) {
-            for (1 .. $max_rows - scalar(@ry)) {
-                push @ry, '0;' x ($data->{$k}->{max_l_2} + 1) . "\n";
-            }
+        while (${$data->{$k}->{rows}}[-1] =~ /[s+]/) {
+           pop @{$data->{$k}->{rows}};
         }
-
-        untie @ry;
-    }
-
-    my $output;
-
-    for my $k (sort {$a <=> $b} keys %$data) {
-        next if not $data->{$k}->{name};
-        my $ln;
-        my $path = $dir . '/'. $data->{$k}->{name};
-        $self->app->log->debug($path);
-
-        tie (my @ry,"Tie::File", $path) or die $!;
-        for (@ry) {
-            $output->{int($ln++)} .= $_;
+        while (scalar(@{$data->{$k}->{rows}}) != $max_rows) {
+            push @{$data->{$k}->{rows}}, '0;'x ($data->{$k}->{max_col} + 1);
         }
-        untie @ry;
     }
 
     open FILE, '>', "$dir/output.csv" or die;
-    for my $k(sort {$a <=> $b} keys %$output) {
-        print FILE $output->{$k} . "\n";
+    my @keys = sort {$a <=> $b} keys %$data;
+    for my $row (0 .. ($max_rows - 1)) {
+        my $str;
+        for my $k (@keys) {
+            my $el = ${$data->{$k}->{rows}}[$row];
+            $str .= $el;
+        }
+        print FILE "$str\n";
     }
     close FILE;
 
